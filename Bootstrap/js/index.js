a75f9d569d87c3a8a0e5595b5aea9060d6634c1d
its-ankit2908 <ap826690@gmail.com>
2021-08-28 08:44:18 +0530

    second update

---
+++ Bootstrap/js/index.js
@@ -0,0 +1,30 @@
+const button = document.querySelector('.ripple');
+const circleButton = document.querySelector('.ripple-circle');
+
+circleButton.addEventListener('click',function(e){
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+
+    let ripples = document.createElement('span');
+    ripples.style.left = x + 'px';
+    ripples.style.top = y + 'px';
+    this.appendChild(ripples);
+
+    setTimeout(() =>{
+        ripples.remove()
+    },1000);
+})
+
+button.addEventListener('click',function(e){
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+
+    let ripples = document.createElement('span');
+    ripples.style.left = x + 'px';
+    ripples.style.top = y + 'px';
+    this.appendChild(ripples);
+
+    setTimeout(() =>{
+        ripples.remove()
+    },1000);
+})
